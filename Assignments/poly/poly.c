baba5f006823d61b0229529138e88e2dadb7d381
filip.lennhager.4154 <fi4154le-s@student.lu.se>
2024-01-07 19:16:43 +0100

    added skeleton for poly implementation

--- Assignments/poly/poly.c
+++ Assignments/poly/poly.c
@@ -5,3 +5,27 @@
 
 #include "error.h"
 #include "poly.h"
+
+struct poly_t {
+    int* coeffs;
+    int* exps;
+};
+
+poly_t*     new_poly_from_string(const char* string){
+    poly_t* p = malloc(sizeof(poly_t));
+    return p;
+}
+
+void        free_poly(poly_t* p){
+    free(p->coeffs);
+    free(p->exps);
+}
+
+poly_t*     mul(poly_t* p, poly_t* q){
+    poly_t* result = malloc(sizeof(poly_t));
+    return result;
+}
+
+void        print_poly(poly_t* p){
+    printf("test\n");
+}
